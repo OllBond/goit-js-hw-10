3017ddae2d3ea7210a3d53a4812599ffa4a1fd7f
Olena <newsroommy@gmail.com>
2022-12-03 17:01:36 +0200

    index.js, fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,11 +1,27 @@
+import axios from 'axios';
+
 // іменований експорт функції
+
 export function fetchCountries(name) {
-  return fetch(
-    'https://restcountries.com/v3.1/name/${name}?fields=name.official,capital,population,flags.svg,languages'
-  )
-    .then(response => response.json())
-    .then(data => console.log(date))
-    .catch(error => {
-      console.log(error);
-    });
+  return axios
+    .get(
+      `https://restcountries.com/v3.1/name/${name}?fields=name.official,capital,population,flags.svg,languages`
+    )
+    .then(({ data }) => data);
 }
+
+// // іди принеси список всіх країн
+// const r = fetch('https://restcountries.com/v3.1/all')
+//   .then(response => {
+//     //  коли принесеш спробуй розпарсити, результат вертаю
+//     // у наступний then
+//     return response.json();
+//   })
+//   // тут те що повернув return якщо все добре вертається масив
+//   .then(all => {
+//     console.log(all);
+//   })
+//   // якщо не добре виконуй помилку
+//   .catch(error => {
+//     console.log(error);
+//   });
--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 // іменований імпорт функції
 import { fetchCountries } from './fetchCountries';
+import debounce from 'lodash.debounce';
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+let inputValue = '';
 
 const refs = {
   inputRef: document.querySelector('#search-box'),
   listRef: document.querySelector('.country-list'),
   countryInfoRef: document.querySelector('.country-info'),
 };
+refs.inputRef.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+
+function onInput(e) {
+  e.preventDefault();
+  inputValue = e.target.value.trim('');
+  // if (inputValue === '') {
+  // }
+}
